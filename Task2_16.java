231f09733c8babd1c8c3e276ead7f80b9b4b4100
hisashi-ueda-road <140694936+hisashi-ueda-road@users.noreply.github.com>
2023-12-15 14:24:59 +0900

    Add files via upload

--- Task2_16.java
+++ Task2_16.java
@@ -7,14 +7,12 @@ public class Task2_16 {
       //① firstNameとlastNameという名前の変数を定義し、
         // firstNameには自分の名前、lastNameには自分の名字で初期化しなさい
         String firstName ="上田";
-        String lastName="寿史";
-        
-        String getName=firstName+lastName;
+        String lastName ="寿史";
         
         
 
         //getName関数の呼び出しと出力
-        System.out.println(getName);
+        System.out.println(getName(firstName,lastName));
         System.out.println();
         
         
@@ -23,7 +21,11 @@ public class Task2_16 {
 
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         // 要素が奇数の場合には「nは奇数です。」と出力されます。
-
+        for (int i = 0; i < arr.length; i++) {
+            if (isOdd(arr[i])) {
+                System.out.println(arr[i] + " は奇数です。");
+            } 
+        }
     }
 
     //② 関数を定義しなさい。
@@ -31,16 +33,12 @@ public class Task2_16 {
     // 修飾子：public
     // 引数には①で定義したfirstNameとlastNameを引数で受け取り、
     // 連結した値を戻り値とする変数を定義しなさい。
-    public static String getName = (String firstName,String lastName) {
-        return firstName + lastName;
+           public static String getName (String firstName, String lastName) {
+            return firstName + lastName;
     }
 
     //奇数の判定を行う。
-    public static void isOdd(int num) {
-        if(num % 2 != 0) {
-            System.out.println(num + "は奇数です。");
-        }
-    }
-
-    }
-
+           public static boolean isOdd(int number) {
+               return number % 2 != 0;
+           }
+       }
