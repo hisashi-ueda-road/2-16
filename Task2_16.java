a4e7b33645e2d4d3bfa27beef6fe990b97534d96
hisashi-ueda-road <140694936+hisashi-ueda-road@users.noreply.github.com>
2023-12-20 12:59:19 +0900

    Add files via upload

--- Task2_16.java
+++ Task2_16.java
@@ -21,10 +21,8 @@ public class Task2_16 {
 
         //③ arr 配列をループさせ、isOdd関数を使って配列の要素が奇数かどうかの判定を行いなさい。
         // 要素が奇数の場合には「nは奇数です。」と出力されます。
-        for (int i = 0; i < arr.length; i++) {
-            if (isOdd(arr[i])) {
-                System.out.println(arr[i] + " は奇数です。");
-            } 
+        for (int i = 0; i < arr.length;i++) {
+           isOdd(arr[i]);
         }
     }
 
@@ -38,7 +36,10 @@ public class Task2_16 {
     }
 
     //奇数の判定を行う。
-           public static boolean isOdd(int number) {
-               return number % 2 != 0;
+           public static void isOdd(int num) {
+               if(num % 2 != 0) {
+                   System.out.println(num + "は奇数です。");
+               }
            }
-       }
+}
+               
